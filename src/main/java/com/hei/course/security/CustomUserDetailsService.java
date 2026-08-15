61e4ba007b59530f94124b55a0358bce1f986933
Miriasme <asminerazafiarivelo@gmail.com>
2026-08-15 10:48:02 +0300

    feat(Security): add security configuration

---
+++ src/main/java/com/hei/course/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.hei.course.security;
+
+
+import com.hei.course.entity.JUsers;
+import com.hei.course.mapper.UserMapper;
+import com.hei.course.model.Users;
+import com.hei.course.repository.JUsersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final JUsersRepository usersRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        JUsers entity = usersRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Email not found: " + email));
+
+        Users user = UserMapper.toModel(entity);
+
+        return new UserPrincipal(user);
+    }
+}
