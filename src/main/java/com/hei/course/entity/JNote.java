0b2cd8cc986b5a24fabc6ab24261dda484f387ff
Miriasme <asminerazafiarivelo@gmail.com>
2026-08-18 00:30:36 +0300

    feat(Entity): add Builder on JNote and JTeacher

--- src/main/java/com/hei/course/entity/JNote.java
+++ src/main/java/com/hei/course/entity/JNote.java
@@ -36,7 +36,6 @@ public class JNote {
   @JoinColumn(name = "student_id", nullable = false)
   private JStudent student;
 
-
   @Builder.Default
   @OneToMany(mappedBy = "note", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<JNoteHistory> noteHistoryList = new ArrayList<>();
--- src/main/java/com/hei/course/entity/JStudent.java
+++ src/main/java/com/hei/course/entity/JStudent.java
@@ -3,7 +3,6 @@ package com.hei.course.entity;
 import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
