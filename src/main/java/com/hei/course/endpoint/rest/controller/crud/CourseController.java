d46d048d2f3f2109e0d419a5a007ea98af9fc3d5
Miriasme <asminerazafiarivelo@gmail.com>
2026-08-17 22:00:41 +0300

    feat(Courses): add endpoints crud for courses

---
+++ src/main/java/com/hei/course/endpoint/rest/controller/crud/CourseController.java
@@ -0,0 +1,45 @@
+package com.hei.course.endpoint.rest.controller.crud;
+
+import com.hei.course.model.Courses;
+import com.hei.course.service.crud.CourseService;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/courses")
+@RequiredArgsConstructor
+public class CourseController {
+
+    private final CourseService courseService;
+
+    @PostMapping
+    public ResponseEntity<Courses> create(@RequestBody Courses course) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseService.create(course));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Courses>> findAll() {
+        return ResponseEntity.ok(courseService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Courses> findById(@PathVariable UUID id) {
+        return ResponseEntity.ok(courseService.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Courses> update(
+            @PathVariable UUID id, @RequestBody Courses course) {
+        return ResponseEntity.ok(courseService.update(id, course));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        courseService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
