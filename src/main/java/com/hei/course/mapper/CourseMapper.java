cf963a307cd10ba8bae0841a15a051c75f975a41
Miriasme <asminerazafiarivelo@gmail.com>
2026-08-17 22:03:21 +0300

    feat(Mapper):  add mapper for courses

--- src/main/java/com/hei/course/mapper/CourseMapper.java
+++ src/main/java/com/hei/course/mapper/CourseMapper.java
@@ -5,25 +5,25 @@ import com.hei.course.model.Courses;
 
 public class CourseMapper {
 
-    private CourseMapper() {}
+  private CourseMapper() {}
 
-    public static Courses toModel(JCourses entity) {
-        return Courses.builder()
-                .id(entity.getId())
-                .reference(entity.getReference())
-                .title(entity.getTitle())
-                .credit(entity.getCredit())
-                .build();
-    }
+  public static Courses toModel(JCourses entity) {
+    return Courses.builder()
+        .id(entity.getId())
+        .reference(entity.getReference())
+        .title(entity.getTitle())
+        .credit(entity.getCredit())
+        .build();
+  }
 
-    public static JCourses toEntity(Courses model) {
-        JCourses entity = new JCourses();
+  public static JCourses toEntity(Courses model) {
+    JCourses entity = new JCourses();
 
-        entity.setId(model.getId());
-        entity.setReference(model.getReference());
-        entity.setTitle(model.getTitle());
-        entity.setCredit(model.getCredit());
+    entity.setId(model.getId());
+    entity.setReference(model.getReference());
+    entity.setTitle(model.getTitle());
+    entity.setCredit(model.getCredit());
 
-        return entity;
-    }
+    return entity;
+  }
 }
