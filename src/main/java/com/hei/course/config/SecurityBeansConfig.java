dbdf44288769bd752d81478ad45995f44f7e2764
Miriasme <asminerazafiarivelo@gmail.com>
2026-08-18 09:58:53 +0300

    refactor(SecurityConfig): remove authentificator attribut

--- src/main/java/com/hei/course/config/SecurityBeansConfig.java
+++ src/main/java/com/hei/course/config/SecurityBeansConfig.java
@@ -2,7 +2,6 @@ package com.hei.course.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,7 +9,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,21 +21,7 @@ public class SecurityBeansConfig {
   }
 
   @Bean
-  public AuthenticationEntryPoint authenticationEntryPoint() {
-    return (request, response, authException) -> {
-      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-      response.setContentType("application/json");
-      response
-          .getWriter()
-          .write(
-              "{\"status\":401,\"error\":\"Unauthorized\",\"message\":\"Authentication"
-                  + " required\"}");
-    };
-  }
-
-  @Bean
-  public SecurityFilterChain filterChain(
-      HttpSecurity http, AuthenticationEntryPoint authenticationEntryPoint) throws Exception {
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
     http.csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(
@@ -132,8 +116,6 @@ public class SecurityBeansConfig {
                     .hasRole("ADMIN")
                     .anyRequest()
                     .authenticated())
-        .exceptionHandling(
-            exception -> exception.authenticationEntryPoint(authenticationEntryPoint))
         .httpBasic(withDefaults());
 
     return http.build();
