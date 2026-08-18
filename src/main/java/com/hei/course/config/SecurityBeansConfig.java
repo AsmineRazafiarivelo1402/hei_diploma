dd2dfe6a66b2ead599bd632622bf2ffb33b5aae3
Miriasme <asminerazafiarivelo@gmail.com>
2026-08-18 10:47:00 +0300

    fix(Security): resolve permission

--- src/main/java/com/hei/course/config/SecurityBeansConfig.java
+++ src/main/java/com/hei/course/config/SecurityBeansConfig.java
@@ -2,7 +2,6 @@ package com.hei.course.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,10 +9,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
-// security
 @Configuration
 @EnableMethodSecurity
 public class SecurityBeansConfig {
@@ -137,8 +134,6 @@ public class SecurityBeansConfig {
                     .hasRole("ADMIN")
                     .anyRequest()
                     .authenticated())
-        .exceptionHandling(
-            exception -> exception.authenticationEntryPoint(authenticationEntryPoint))
         .httpBasic(withDefaults());
 
     return http.build();
