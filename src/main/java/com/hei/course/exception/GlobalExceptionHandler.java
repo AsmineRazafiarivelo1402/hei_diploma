5f774e7f8f3417e966daeb5e36629b43ce38299c
Miriasme <asminerazafiarivelo@gmail.com>
2026-08-19 14:48:59 +0300

    feat(Exception): add exception handler

--- src/main/java/com/hei/course/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hei/course/exception/GlobalExceptionHandler.java
@@ -2,11 +2,16 @@ package com.hei.course.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import java.time.Instant;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -31,9 +36,27 @@ public class GlobalExceptionHandler {
     return buildResponse(HttpStatus.CONFLICT, exception.getMessage(), request.getRequestURI());
   }
 
+  @ExceptionHandler(AccessDeniedException.class)
+  public ResponseEntity<ErrorResponse> handleAccessDenied(
+      AccessDeniedException exception, HttpServletRequest request) {
+
+    return buildResponse(HttpStatus.FORBIDDEN, exception.getMessage(), request.getRequestURI());
+  }
+
+  @ExceptionHandler({NoResourceFoundException.class, NoHandlerFoundException.class})
+  public ResponseEntity<ErrorResponse> handleRouteNotFound(
+      Exception exception, HttpServletRequest request) {
+
+    return buildResponse(
+        HttpStatus.NOT_FOUND, "No endpoint found for this request", request.getRequestURI());
+  }
+
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ErrorResponse> handleGenericException(
       Exception exception, HttpServletRequest request) {
+
+    log.error(
+        "Unhandled exception on {} {}", request.getMethod(), request.getRequestURI(), exception);
 
     return buildResponse(
         HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurred", request.getRequestURI());
